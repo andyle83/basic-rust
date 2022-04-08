72136aa95f2adbd5a99e71bde294600c5f5fea5e
Tuan Anh Le <anh.le@myob.com>
2022-04-08 15:32:18 +1000

    feat: pointer type

--- src/main.rs
+++ src/main.rs
@@ -51,3 +51,29 @@ fn tuple_type_check() {
     assert_eq!(head, "12");
     assert_eq!(tail, "345");
 }
+
+#[test]
+fn pointer_type_check() {
+    // pointer represent address of a memory (heap, stack)
+    // types of pointer
+    // 1. reference
+    // 2. boxes
+    // 3. unsafe pointer
+
+    // 1. reference
+    // Giving a x value
+    // &x produce a reference to x - borrow reference to x
+    // NO way to create a NULL reference
+    // 1. &x - immutable reference or read only
+    // 2. &mut x - mutable reference with write permit
+    let x = 10;
+    let r = &x;
+    let p = &x;
+    assert_eq!(r, p);
+    assert_eq!(*r, 10);
+
+    // 2. boxes
+    // Giving a way to allocate value in head
+    let profile = (40, "Tuan Anh");
+    let profile_box = Box::new(profile);
+}
