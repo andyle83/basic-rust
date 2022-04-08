f35fe7d1df1cfad7ac1737396183d0237a8451c1
Tuan Anh Le <anh.le@myob.com>
2022-04-08 15:03:15 +1000

    feat: character type

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,16 @@ fn byte_literal_check() {
 fn char_type_check() {
     // char - represent single Unicode character - 32 bits
     // String - sequence of UTF-8 bytes, not array of character
+
+    // decimal
     assert_eq!('a' as i32, 97);
     assert_eq!('A' as i32, 65);
+
+    // hex
     assert_eq!('a' as i32, 0x61);
     assert_eq!('A' as i32, 0x41);
+
+    // oct
+    assert_eq!('a' as i32, 0o0141);
+    assert_eq!('A' as i32, 0o0101);
 }
