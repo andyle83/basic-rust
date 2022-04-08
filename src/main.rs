308b8ef96e46a40cee249002a817e8d909fd2a6f
Tuan Anh Le <anh.le@myob.com>
2022-04-08 15:14:40 +1000

    feat: tuple types

--- src/main.rs
+++ src/main.rs
@@ -32,4 +32,22 @@ fn char_type_check() {
     // oct
     assert_eq!('a' as i32, 0o0141);
     assert_eq!('A' as i32, 0o0101);
+
+    // method in character
+    assert_eq!('a'.is_alphabetic(), true);
+    assert_eq!('8'.to_digit(10), Some(8));
+    assert_eq!(std::char::from_digit(8, 10), Some('8'));
+}
+
+#[test]
+fn tuple_type_check() {
+    // Type: group of different value with different types
+    let text = "12345";
+    let (head, tail) = text.split_at(2);
+
+    println!("head {:?}", head);
+    println!("tail {:?}", tail);
+
+    assert_eq!(head, "12");
+    assert_eq!(tail, "345");
 }
