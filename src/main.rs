a4f48db0aa78e47e540b7d178451c6aab7426813
Tuan Anh Le <anh.le@myob.com>
2022-04-09 13:30:23 +1000

    feat: code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,6 @@
+mod r#type;
+
 fn main() {
-}
-
-#[test]
-fn byte_literal_check() {
-    // byte literal - ASC characters only
-    // emphasize value represent ASC character
-    let a = b'a';
-    let A = b'A';
-
-    println!("{:?}", a);
-    println!("{:?}", A);
-    println!("{:?}", A);
-
-    assert_eq!(a, 97);
-    assert_eq!(A, 65);
-}
-
-#[test]
-fn char_type_check() {
-    // char - represent single Unicode character - 32 bits
-    // String - sequence of UTF-8 bytes, not array of character
-
-    // decimal
-    assert_eq!('a' as i32, 97);
-    assert_eq!('A' as i32, 65);
-
-    // hex
-    assert_eq!('a' as i32, 0x61);
-    assert_eq!('A' as i32, 0x41);
-
-    // oct
-    assert_eq!('a' as i32, 0o0141);
-    assert_eq!('A' as i32, 0o0101);
-
-    // method in character
-    assert_eq!('a'.is_alphabetic(), true);
-    assert_eq!('8'.to_digit(10), Some(8));
-    assert_eq!(std::char::from_digit(8, 10), Some('8'));
-}
-
-#[test]
-fn tuple_type_check() {
-    // Type: group of different value with different types
-    let text = "12345";
-    let (head, tail) = text.split_at(2);
-
-    println!("head {:?}", head);
-    println!("tail {:?}", tail);
-
-    assert_eq!(head, "12");
-    assert_eq!(tail, "345");
-}
-
-#[test]
-fn pointer_type_check() {
-    // pointer represent address of a memory (heap, stack)
-    // types of pointer
-    // 1. reference
-    // 2. boxes
-    // 3. unsafe pointer
-
-    // 1. reference
-    // Giving a x value
-    // &x produce a reference to x - borrow reference to x
-    // NO way to create a NULL reference
-    // 1. &x - immutable reference or read only
-    // 2. &mut x - mutable reference with write permit
-    {
-        let x = 10;
-        let r = &x;
-        let p = &x;
-        assert_eq!(r, p);
-        assert_eq!(*r, 10);
-    }
-
-    // 2. boxes
-    // Giving a way to allocate value in head
-    {
-        let profile = (40, "Tuan Anh");
-        let profile_box = Box::new(profile);
-        let (age, name) = *profile_box;
-        assert_eq!(age, 40);
-        assert_eq!(name, "Tuan Anh");
-    }
-
-    // 3. raw point
-    // no tracking
-    // may be null
-    // may point to free memory
 }
 
 #[test]
@@ -124,7 +37,7 @@ fn vector_type_check() {
     }
 
     // another way to create vector
-    let mut n: Vec<i32> = (1..3).collect();
+    let n: Vec<i32> = (1..3).collect();
     let f = n.iter().fold(0, | s1, s2 | s1 + s2 );
     assert_eq!(f, 3);
 
@@ -173,4 +86,43 @@ fn slice_type_check() {
 
     // get few elements by slice
     println!("{:?}",&v[0..2]);
+}
+
+#[test]
+fn string_check_types() {
+    // By https://doc.rust-lang.org/, there are two types of string
+    // 1. String literal
+    // 2. &str: string slice
+
+    // 1. String literal
+    // - vector of bytes Vec<u8> - UTF8 sequence
+    // - heap allocation
+    // - because it's vector of bytes, its fat-pointer include
+    // + pointer to address
+    // + capacity
+    // + actual numbers of element (depending)
+
+    // represent by double quotes
+    let first_speech = "Good morning Melbourne \n".to_string();
+    println!("{}", first_speech);
+
+    // represent with raw
+    let second_speech = r"Rust compiler path - C:\Program Files\Rust".to_string();
+    println!("{}", second_speech);
+
+    // represent by byte string - look like a slice of an array
+    let third_speech = b"GET";
+    let fourth_speed = &[b'G', b'E', b'T'];
+    assert_eq!(third_speech, fourth_speed);
+
+    // 2. string slice
+    // Reference (borrow reference) to UTF-8 owned by something else
+    // Fat pointer, contain:
+    // - Pointer
+    // - Its length
+    let fifth_speech = &first_speech[0..4];
+    let sixth_speech = "Good";
+    assert_eq!(fifth_speech, sixth_speech);
+    // len - number of byte
+    assert_eq!(sixth_speech.len(), 4);
 }
---
+++ src/type.rs
@@ -0,0 +1,90 @@
+#[cfg(test)]
+mod types {
+    #[test]
+    fn byte_literal_check() {
+        // byte literal - ASC characters only
+        // emphasize value represent ASC character
+        let a = b'a';
+        let capital_a = b'A';
+
+        println!("{:?}", a);
+        println!("{:?}", capital_a);
+
+        assert_eq!(a, 97);
+        assert_eq!(capital_a, 65);
+    }
+
+    #[test]
+    fn char_type_check() {
+        // char - represent single Unicode character - 32 bits
+        // String - sequence of UTF-8 bytes, not array of character
+
+        // decimal
+        assert_eq!('a' as i32, 97);
+        assert_eq!('A' as i32, 65);
+
+        // hex
+        assert_eq!('a' as i32, 0x61);
+        assert_eq!('A' as i32, 0x41);
+
+        // oct
+        assert_eq!('a' as i32, 0o0141);
+        assert_eq!('A' as i32, 0o0101);
+
+        // method in character
+        assert_eq!('a'.is_alphabetic(), true);
+        assert_eq!('8'.to_digit(10), Some(8));
+        assert_eq!(std::char::from_digit(8, 10), Some('8'));
+    }
+
+    #[test]
+    fn tuple_type_check() {
+        // Type: group of different value with different types
+        let text = "12345";
+        let (head, tail) = text.split_at(2);
+
+        println!("head {:?}", head);
+        println!("tail {:?}", tail);
+
+        assert_eq!(head, "12");
+        assert_eq!(tail, "345");
+    }
+
+    #[test]
+    fn pointer_type_check() {
+        // pointer represent address of a memory (heap, stack)
+        // types of pointer
+        // 1. reference
+        // 2. boxes
+        // 3. unsafe pointer
+
+        // 1. reference
+        // Giving a x value
+        // &x produce a reference to x - borrow reference to x
+        // NO way to create a NULL reference
+        // 1. &x - immutable reference or read only
+        // 2. &mut x - mutable reference with write permit
+        {
+            let x = 10;
+            let r = &x;
+            let p = &x;
+            assert_eq!(r, p);
+            assert_eq!(*r, 10);
+        }
+
+        // 2. boxes
+        // Giving a way to allocate value in head
+        {
+            let profile = (40, "Tuan Anh");
+            let profile_box = Box::new(profile);
+            let (age, name) = *profile_box;
+            assert_eq!(age, 40);
+            assert_eq!(name, "Tuan Anh");
+        }
+
+        // 3. raw point
+        // no tracking
+        // may be null
+        // may point to free memory
+    }
+}
