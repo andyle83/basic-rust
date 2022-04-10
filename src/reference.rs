7e987222844b0b1508f62a4b348ab0c7124b7dac
Tuan Anh Le <anh.le@myob.com>
2022-04-11 09:22:57 +1000

    fix: using mutable ref

--- src/reference.rs
+++ src/reference.rs
@@ -20,9 +20,15 @@ fn show_table_by_reference(table: &Table) {
     }
 }
 
+fn sort_table(table: &mut Table) {
+    for (_kid, works) in table {
+        works.sort();
+    }
+}
+
 #[cfg(test)]
 mod reference {
-    use crate::reference::{show_table, show_table_by_reference, Table};
+    use crate::reference::{show_table, show_table_by_reference, sort_table, Table};
 
     # [test]
     fn problem_with_move_ownership() {
@@ -39,7 +45,7 @@ mod reference {
     }
 
     # [test]
-    fn solve_problem_with_shared_reference() {
+    fn test_with_shared_ref() {
         let mut table = Table::new();
         table.insert("Duc Minh".to_string(),
                      vec!["studying".to_string(), "playing piano".to_string()]);
@@ -50,6 +56,19 @@ mod reference {
 
         // hashmap object has moved to function, so it can not be used
         assert_eq!(table["Ngoc Minh"][0], "writing".to_string());
+    }
+
+    #[test]
+    fn test_with_mutable_ref() {
+        let mut table = Table::new();
+        table.insert("Duc Minh".to_string(),
+                     vec!["studying".to_string(), "playing piano".to_string()]);
+        table.insert("Ngoc Minh".to_string(),
+                     vec!["writing".to_string(), "cleaning".to_string()]);
+
+        sort_table(&mut table);
+
+        assert_eq!(table["Ngoc Minh"][0], "cleaning".to_string());
     }
 
 }
