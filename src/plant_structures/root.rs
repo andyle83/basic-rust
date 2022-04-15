a1fdd5da89bccbb85133cd7d352377337832030f
Tuan Anh Le <anh.le@myob.com>
2022-04-15 18:57:38 +1000

    feat: using function imported from module

--- src/plant_structures/root.rs
+++ src/plant_structures/root.rs
@@ -1,3 +1,4 @@
+use crate::plant_structures::branch;
 use crate::plant_structures::branch::Branch;
 use crate::plant_structures::leave::Leave;
 
@@ -5,7 +6,8 @@ pub struct Root {
     pub node: Branch
 }
 
-pub fn init_root() -> Root {
+#[test]
+pub fn init_root() {
     let l1 = Leave {
         color: "blue".to_string(),
     };
@@ -14,9 +16,13 @@ pub fn init_root() -> Root {
         color: "yellow".to_string(),
     };
 
-    let b = Branch {
+    let b = &mut Branch {
         leaves: Vec::new(),
     };
 
     // invoke fn from branch module to add leaves l1 and l2
+    branch::add_leaves(b, l1);
+    branch::add_leaves(b, l2);
+
+    assert_eq!(b.leaves.len(), 2);
 }
