eb67c2e643552a8d5c8a697afbff519b0c7a1003
Tuan Anh Le <anh.le@myob.com>
2022-04-16 22:13:28 +1000

    feat: convert int to enum type

--- src/enums.rs
+++ src/enums.rs
@@ -4,6 +4,15 @@ mod enums {
     use http::StatusCode;
     use std::mem::size_of;
 
+    fn http_status_from_u32(n: u32) -> Option<StatusCode> {
+        match n {
+            200 => Some(StatusCode::OK),
+            304 => Some(StatusCode::NOT_MODIFIED),
+            404 => Some(StatusCode::NOT_FOUND),
+            _ => None
+        }
+    }
+
     #[test]
     fn test_builtin_enum() {
         assert_eq!(size_of::<Ordering>(), 1);
@@ -14,5 +23,11 @@ mod enums {
         assert_eq!(size_of::<StatusCode>(), 2);
         assert_eq!(StatusCode::OK, 200);
         assert_eq!(StatusCode::NOT_FOUND, 404);
+    }
+
+    #[test]
+    fn test_status_code_converter() {
+        assert_eq!(http_status_from_u32(200).unwrap(), StatusCode::OK);
+        assert_eq!(http_status_from_u32(100), None);
     }
 }
