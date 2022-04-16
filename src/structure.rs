72ccea0893acebe08cc9df5317bad797420ab104
Tuan Anh Le <anh.le@myob.com>
2022-04-16 21:35:58 +1000

    feat: implement structure method

--- src/structure.rs
+++ src/structure.rs
@@ -6,6 +6,15 @@ mod structure {
         size: (usize, usize)
     }
 
+    impl GrayscaleMap {
+        pub fn friendly_print(&self, label: &str) {
+            println!("*** image {:} ***", label);
+            println!("pixels size {}", self.pixels.len());
+            println!("size {:?}", self.size);
+            println!("*** end ***");
+        }
+    }
+
     // shorthand syntax for structure
     fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
         assert_eq!(pixels.len(), size.0 * size.1);
@@ -20,12 +29,14 @@ mod structure {
             pixels: vec![0; width * height],
             size: (width, height)
         };
-
         assert_eq!(image1.pixels.len(), width * height);
 
         let image2 = new_map((width, height), vec![1; width * height]);
-
         assert_eq!(image2.pixels.len(), width * height);
+
+        // calling struct method
+        image1.friendly_print("image1");
+        image2.friendly_print("image2");
     }
 
 }
