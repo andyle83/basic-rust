ab8c730bb44c7d2df5fee656dc420bd4586ed9fd
Tuan Anh Le <anh.le@myob.com>
2022-04-16 22:07:50 +1000

    feat: adding sample of built enum types.

--- src/enums.rs
+++ src/enums.rs
@@ -1,4 +1,18 @@
 #[cfg(test)]
 mod enums {
+    use std::cmp::Ordering;
+    use http::StatusCode;
+    use std::mem::size_of;
 
+    #[test]
+    fn test_builtin_enum() {
+        assert_eq!(size_of::<Ordering>(), 1);
+        assert_eq!(Ordering::Less as i32, -1);
+        assert_eq!(Ordering::Equal as i32, 0);
+        assert_eq!(Ordering::Greater as i32, 1);
+
+        assert_eq!(size_of::<StatusCode>(), 2);
+        assert_eq!(StatusCode::OK, 200);
+        assert_eq!(StatusCode::NOT_FOUND, 404);
+    }
 }
