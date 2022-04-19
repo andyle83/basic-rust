ca51cde3a356317146579e9293aa0a09926fb46d
Tuan Anh Le <anh.le@myob.com>
2022-04-19 14:52:41 +1000

    feat: enum contain struct variant

--- src/enums.rs
+++ src/enums.rs
@@ -60,4 +60,61 @@ mod enums {
         assert_eq!(hours.plural(), "hours");
         assert_eq!(hours.singular(), "hour");
     }
+
+    // Example data contain enum
+    #[derive(Copy, Clone, Debug, PartialEq)]
+    enum RoughTime {
+        InThePass(TimeUnit, u32),
+        JustNow,
+        InTheFuture(TimeUnit, u32)
+    }
+
+    impl RoughTime {
+        fn print_rough_time(self) -> String {
+            match self {
+                RoughTime::JustNow => "Just in time".to_string(),
+                RoughTime::InThePass(unit, length) => format!("Last {:?} {:?}", length, unit.plural()),
+                RoughTime::InTheFuture(unit, length) => format!("Until {:?} {:?}", length, unit.plural())
+            }
+        }
+    }
+
+    #[test]
+    fn test_rough_time() {
+        let three_hours_from_now = RoughTime::InTheFuture(TimeUnit::Hours, 3);
+        println!("{}", three_hours_from_now.print_rough_time());
+    }
+
+
+    // Struct variant inside enum
+    struct Point3D {
+        x: u32,
+        y: u32,
+        z: u32
+    }
+
+    enum Shape {
+        Sphere {
+            center: Point3D,
+            radius: u32
+        },
+        Cuboid {
+            corner1: Point3D,
+            corner2: Point3D
+        }
+    }
+
+    #[test]
+    fn enum_contain_struct_variant() {
+        let shape = Shape::Sphere {
+            center: Point3D {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            radius: 100
+        };
+
+        // How to unpack Shape to get radius value
+    }
 }
