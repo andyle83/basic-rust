6f0d89b7a26a253562107f082c8ace1f6b7099c2
Tuan Anh Le <anh.le@myob.com>
2022-04-21 06:03:42 +1000

    fix: issue of using trait for local file writer

--- src/traits.rs
+++ src/traits.rs
@@ -2,23 +2,22 @@
 mod traits {
     use std::fs::File;
     use std::io::Write;
-    use std::io::Result;
 
     // Using generic function with trait
-    fn say_hello<W: Write>(out: &mut W) -> Result<()> {
+    fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
         out.write_all(b"Aa");
         out.flush()
     }
 
     #[test]
     fn test_say_hello() {
-        let mut local_file = File::create("hello.txt");
+        let mut local_file = File::create("hello.txt").expect("Unable to create file");
         let mut bytes: Vec<u8> = vec![];
 
         say_hello(&mut bytes);
-        // say_hello(&mut local_file);
+        say_hello(&mut local_file);
 
-        println!("Value in buf {:?}", bytes);
+        // println!("Value in buf {:?}", bytes);
     }
 
     trait TaskHandling {
